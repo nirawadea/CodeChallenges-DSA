d841b555f28042d58ebbeaead08d6e69555b2cf3
nirawadea <nirawade.a@northeastern.edu>
2025-06-14 00:03:43 -0400

    Binary search lower bound

--- Test/src/BinarySearch/SearchInsertPosition.java
+++ Test/src/BinarySearch/SearchInsertPosition.java
@@ -1,8 +1,9 @@
 package BinarySearch;
 
 public class SearchInsertPosition {
-//Use lower bound
-
+    //Use lower bound
+    //     int[] numsArray = {1,3,5,6};
+    //     int tval = 2;
     public int searchInsert(int[] nums, int target){
 
         int n = nums.length;
@@ -13,14 +14,15 @@ public class SearchInsertPosition {
         while(left<right){
 
             int mid = (left+right)/2;
-
+            // maybe answer
             if(nums[mid] >= target){
                 ans = mid;
+                //check for more smaller element
                 right = mid-1;
             }else{
+                //look for right side
                 left = mid+1;
             }
-
         }
         return ans;
     }
